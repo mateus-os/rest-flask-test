e505fc47d27de5e851876627fc499b13dc1bc244
mtsde-96 <mtsde-96@users.noreply.github.com>
2020-07-30 22:45:06 -0300

    First commit, added REST API Flask

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt import JWT
+
+from db import db
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'mateus'
+api = Api(app)
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity)
+
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+        'access_token': access_token.decode('utf-8'),
+        'user_id': identity.id
+    })
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=80, debug=True)
