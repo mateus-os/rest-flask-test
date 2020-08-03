7257ae64a5640aa949dd7ac10b2ca8ac4be6c9eb
mtsde-96 <mtsde-96@users.noreply.github.com>
2020-08-03 19:26:09 -0300

    Little fixes

--- resources/item.py
+++ resources/item.py
@@ -63,4 +63,4 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'items': [item.json() for item in ItemModel.query.all()]}
+        return {'items': [item.json() for item in ItemModel.find_all()]}
